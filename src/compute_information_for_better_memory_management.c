7aa3fd0674c269eedf0b647956f91843c2bd9281
sagnikbanerjee15 <sagnik.banerjee@bms.com>
2022-11-27 18:29:52 -0800

    push

--- src/compute_information_for_better_memory_management.c
+++ src/compute_information_for_better_memory_management.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <argp.h>
-#include "htslib/htslib/sam.h"
-#include "htslib/sam.c"
+#include "htslib-1.16/htslib/sam.h"
+#include "htslib-1.16/sam.c"
 #include "data_structure_definitions.h"
 #include "function_definitions.h"
 
