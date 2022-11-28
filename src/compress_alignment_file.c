06771f040cc7b3e3b5242d4a9f5228d942a529d9
sagnikbanerjee15 <sagnik.banerjee@bms.com>
2022-11-27 17:23:16 -0800

    push

--- src/compress_alignment_file.c
+++ src/compress_alignment_file.c
@@ -163,7 +163,7 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
 static struct argp argp =
     {options, parse_opt, args_doc, doc, 0, 0, 0};
 
-int main()
+int main(int argc, char *argv[])
 {
     /********************************************************************
      * Named CLI
